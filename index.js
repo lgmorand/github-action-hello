6d3dc891892524f53f8452314e328f150f5ad8d7
Louis-Guillaume MORAND <lgm@lgmorand.com>
2022-11-20 11:02:30 +0100

    adding parameter

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const core = require('@actions/core');
 try 
 {
   const firstname = core.getInput('firstname'); 
-  console.log(`Hello ${firstname}`);  
+  const firstname2 = core.getInput('firstname2'); 
+  console.log(`Hello ${firstname} & ${firstname2}`);  
 } 
 catch (error) 
 {
