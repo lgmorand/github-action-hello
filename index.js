c5cec49230203f171fe56b91a7a8db32b0cd7231
Louis-Guillaume MORAND <lgm@lgmorand.com>
2022-11-20 13:52:12 +0100

    v2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 try 
 {
   const firstname = core.getInput('firstname'); 
+  const firstname2 = core.getInput('firstname2'); 
   console.log(`Hello ${firstname}`);  
 } 
 catch (error) 
